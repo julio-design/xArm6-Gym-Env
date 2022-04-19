d49a7bb55ec8051ea8320c8303272158384025ad
Julio Cesar RAMIREZ <julio@jcrcx.xyz>
2022-04-19 16:00:36 -0500

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='gym_xarm6',
         author="Julio Ramirez",
         author_email="julio@jcrcx.xyz",
-        description="xArm 6 robot environment for gym",
+        description="xArm 6 robot environment for gym. It also contains a kinematic model of the robot.",
         url="https://github.com/ollintzinlab/xArm6-Gym-Env",
         version='0.0.1',
         licence="MIT",
